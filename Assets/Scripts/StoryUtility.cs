f754220c3f33ab550bf2861a318dfbb251029842
Nova Cravey <1028955@student.sae.edu.au>
2023-10-25 15:29:19 +1100

    Added decor

--- Assets/Scripts/StoryUtility.cs
+++ Assets/Scripts/StoryUtility.cs
@@ -10,6 +10,14 @@ public class StoryUtility : MonoBehaviour
     public GameObject scene1;
     public GameObject scene2;
     public GameObject scene3;
+    public GameObject scene4;
+    public GameObject scene5;
+    public GameObject scene6;
+    public GameObject scene7;
+    public GameObject scene8;
+    public GameObject scene9;
+    public GameObject scene10;
+    public GameObject scene11;
 
     public TextMeshProUGUI nextButton;
 
@@ -20,6 +28,14 @@ public class StoryUtility : MonoBehaviour
         scene1.SetActive(false);
         scene2.SetActive(false);
         scene3.SetActive(false);
+        scene4.SetActive(false);
+        scene5.SetActive(false);
+        scene6.SetActive(false);
+        scene7.SetActive(false);
+        scene8.SetActive(false);
+        scene9.SetActive(false);
+        scene10.SetActive(false);
+        scene11.SetActive(false);
 
         sceneIndex++;
 
@@ -34,7 +50,38 @@ public class StoryUtility : MonoBehaviour
         else if (sceneIndex == 3)
         {
             scene3.SetActive(true);
-            nextButton.text = "PLAY";
+        }
+        else if (sceneIndex == 4)
+        {
+            scene4.SetActive(true);
+        }
+        else if (sceneIndex == 5)
+        {
+            scene5.SetActive(true);
+        }
+        else if (sceneIndex == 6)
+        {
+            scene6.SetActive(true);
+        }
+        else if (sceneIndex == 7)
+        {
+            scene7.SetActive(true);
+        }
+        else if (sceneIndex == 8)
+        {
+            scene8.SetActive(true);
+        }
+        else if (sceneIndex == 9)
+        {
+            scene9.SetActive(true);
+        }
+        else if (sceneIndex == 10)
+        {
+            scene10.SetActive(true);
+        }
+        else if (sceneIndex == 11)
+        {
+            scene11.SetActive(true);
         }
         else
         {
