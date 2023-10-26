58faaeeef0260d41accaf942f2fca353bca92902
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-10-26 15:23:57 +1100

    Update AtomManager.cs

--- Assets/Scripts/AtomManager.cs
+++ Assets/Scripts/AtomManager.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class AtomManager : MonoBehaviour
 {
+    [Header("Configuration")]
+    [SerializeField] private float yPosition = 5f;
+    [SerializeField] private AtomSize atomSize = AtomSize.Small;
     private Rigidbody rigidBody => GetComponent<Rigidbody>();
+    private PlayerController playerController => GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody.isKinematic = true;
+
     }
 
     // Update is called once per frame
@@ -18,8 +22,50 @@ public class AtomManager : MonoBehaviour
         
     }
 
-    public void SetKinematic(bool isKinematic)
+    public void PickupAtom()
     {
-        rigidBody.isKinematic = isKinematic;
+        switch (atomSize)
+        {
+            case AtomSize.Small:
+                transform.position = playerController.pickupTargetSmall.position;
+                transform.rotation = playerController.pickupTargetSmall.rotation;
+                break;
+            case AtomSize.Medium:
+                transform.position = playerController.pickupTargetMedium.position;
+                transform.rotation = playerController.pickupTargetMedium.rotation;
+                break;
+            case AtomSize.Large:
+                transform.position = playerController.pickupTargetLarge.position;
+                transform.rotation = playerController.pickupTargetLarge.rotation;
+                break;
+            case AtomSize.VeryLarge:
+                transform.position = playerController.pickupTargetVeryLarge.position;
+                transform.rotation = playerController.pickupTargetVeryLarge.rotation;
+                break;
+        }
+        rigidBody.isKinematic = false;
+        HingeJoint hingeJoint = gameObject.AddComponent<HingeJoint>();
+        hingeJoint.connectedBody = playerController.GetComponent<Rigidbody>();
+        hingeJoint.axis = new Vector3(0, 0, 1);
+        hingeJoint.useSpring = true;
+        hingeJoint.spring = new JointSpring() { spring = 10, damper = 1 };
+        hingeJoint.useLimits = true;
+        hingeJoint.limits = new JointLimits() { min = -10, max = 10 };
+    }
+
+    public void DropAtom()
+    {
+        Destroy(GetComponent<HingeJoint>());
+        rigidBody.isKinematic = true;
+        transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
+        transform.rotation = Quaternion.identity;
+    }
+
+    public enum AtomSize
+    {
+        Small,
+        Medium,
+        Large,
+        VeryLarge
     }
 }
