b82de6381775b91f020b6e129e0c3d4ee516262f
Nova Cravey <114209659+Novarcharesk@users.noreply.github.com>
2023-10-26 21:25:28 +1100

    .

--- Assets/Scripts/StoryUtility.cs
+++ Assets/Scripts/StoryUtility.cs
@@ -1,97 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class StoryUtility : MonoBehaviour
 {
+    public GameObject[] scenes;
 
-    public GameObject scene1;
-    public GameObject scene2;
-    public GameObject scene3;
-    public GameObject scene4;
-    public GameObject scene5;
-    public GameObject scene6;
-    public GameObject scene7;
-    public GameObject scene8;
-    public GameObject scene9;
-    public GameObject scene10;
-    public GameObject scene11;
-    public GameObject scene12;
+    private int sceneIndex = 0;
 
-    public TextMeshProUGUI nextButton;
-
-    private int sceneIndex = 1;
+    private void Start()
+    {
+        
+        for (int i = 0; i < scenes.Count(); i++)
+        {
+            HideScene(i);
+        }
+        
+        ShowScene(0);
+    }
 
     public void NextScene()
     {
-        scene1.SetActive(false);
-        scene2.SetActive(false);
-        scene3.SetActive(false);
-        scene4.SetActive(false);
-        scene5.SetActive(false);
-        scene6.SetActive(false);
-        scene7.SetActive(false);
-        scene8.SetActive(false);
-        scene9.SetActive(false);
-        scene10.SetActive(false);
-        scene11.SetActive(false);
-        scene12.SetActive(false);
-
+        HideScene(sceneIndex);
         sceneIndex++;
 
-        if (sceneIndex == 1)
+        if (sceneIndex < scenes.Length)
         {
-            scene1.SetActive(true);
+            ShowScene(sceneIndex);
         }
-        else if (sceneIndex == 2)
+    }
+
+    private void ShowScene(int index)
+    {
+        if (index >= 0 && index < scenes.Length)
         {
-            scene2.SetActive(true);
+            scenes[index].SetActive(true);
         }
-        else if (sceneIndex == 3)
+    }
+
+    private void HideScene(int index)
+    {
+        if (index >= 0 && index < scenes.Length)
         {
-            scene3.SetActive(true);
-        }
-        else if (sceneIndex == 4)
-        {
-            scene4.SetActive(true);
-        }
-        else if (sceneIndex == 5)
-        {
-            scene5.SetActive(true);
-        }
-        else if (sceneIndex == 6)
-        {
-            scene6.SetActive(true);
-        }
-        else if (sceneIndex == 7)
-        {
-            scene7.SetActive(true);
-        }
-        else if (sceneIndex == 8)
-        {
-            scene8.SetActive(true);
-        }
-        else if (sceneIndex == 9)
-        {
-            scene9.SetActive(true);
-        }
-        else if (sceneIndex == 10)
-        {
-            scene10.SetActive(true);
-        }
-        else if (sceneIndex == 11)
-        {
-            scene11.SetActive(true);
-        }
-        else if (sceneIndex == 12)
-        {
-            scene12.SetActive(true);
-        }
-        else
-        {
-            SceneManager.LoadScene("SampleScene");
+            scenes[index].SetActive(false);
         }
     }
 }
